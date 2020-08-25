2071e7a2e29aaded75a6883c8fcc9fe6176d5003
rifaq ahmer <rifaq_ahmer@gmail.com>
2020-08-26 00:00:15 +0530

    Completed-till-add-function

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+  currentResult = currentResult + parseInt(userInput.value);
+  outputResult(currentResult, "");
+}
+addBtn.addEventListener("click", add);
