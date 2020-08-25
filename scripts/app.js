48ddc554f98a227d29880dff6daa5c39f49514ad
rifaq ahmer <rifaq_ahmer@gmail.com>
2020-08-26 00:47:18 +0530

    Compelete Calculator

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,42 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-  currentResult = currentResult + parseInt(userInput.value);
-  outputResult(currentResult, "");
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+function outputDisplay(operator, resultBefore, calcResult) {
+  const calcDescription = `${resultBefore} ${operator} ${calcResult}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  outputDisplay("+", initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  outputDisplay("-", initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  outputDisplay("*", initialResult, enteredNumber);
+}
+
+function division() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  outputDisplay("/", initialResult, enteredNumber);
 }
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", division);
